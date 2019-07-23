850abefa367b4357f9282d4c10b3b7fdcf0e4f8d
chen970226 <cql0226@163.com>
2019-07-23 15:36:05 +0800

    new

---
+++ git_push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+message=${@:1}
+echo $message
+if [ -z "$message" ]
+then 
+	echo "please input message!"
+	exit
+fi
+echo "===============git adding==============="
+git add .
+echo "=============git add completed=============="
+
+echo "===============git commiting==============="
+ 
+git commit -m " $message "
+
+echo "===============git commit completed============"
+
+echo "===============git pushing==============="
+git push origin master
+
+echo "===============git push completed==============="
